a5bad90948f81551afbe3f91570d2162ec25eb40
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-06-29 09:24:32 +1200

    We should be able to a numeric value from a json property

--- src/JsonReader.Tests/ReaderTests.cs
+++ src/JsonReader.Tests/ReaderTests.cs
@@ -17,12 +17,11 @@ namespace JsonReader.Tests
             Assert.AreEqual(expectedValue, reader.GetString(new string[] {property}));
         }
 
-        // [TestCase("{\"id\": 1}", "id", "1")]
-        // public void ShouldGetIntValue(string json, string property, int expectedValue)
-        // {
-
-        //     var reader = new Reader(json);
-        //     Assert.AreEqual(expectedValue, reader.GetInt(new string[] {property}));
-        // }
+        [TestCase("{\"id\": 1}", "id", "1")]
+        public void ShouldGetIntValue(string json, string property, int expectedValue)
+        {
+            var reader = new Reader(json);
+            Assert.AreEqual(expectedValue, reader.GetInt(new string[] {property}));
+        }
     }
 }
--- src/JsonReader/Reader.cs
+++ src/JsonReader/Reader.cs
@@ -11,9 +11,9 @@ namespace JsonReader
             document = JsonDocument.Parse(json);
         }
 
-        // public int GetInt(string[] pathToElements) {
-        //     return document.RootElement.GetProperty(pathToElements[0]).GetInt32();
-        // }
+        public int GetInt(string[] pathToElements) {
+            return document.RootElement.GetProperty(pathToElements[0]).GetInt32();
+        }
 
         public string GetString(string[] pathToElements) {
             return document.RootElement.GetProperty(pathToElements[0]).GetString();
